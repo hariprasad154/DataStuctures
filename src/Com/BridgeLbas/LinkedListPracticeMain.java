198d7aa6d750effe3ff8b3553910d4d027360da9
Hari Prasad G <hariprasas154@gmail.com>
2023-01-31 11:11:43 +0530

    the simple linked list of 3 nums

---
+++ src/Com/BridgeLbas/LinkedListPracticeMain.java
@@ -0,0 +1,17 @@
+package Com.BridgeLbas;
+
+
+
+public class LinkedListPracticeMain {
+	public static void main(String[] args) {
+		
+	
+	LinkedListPractice<Integer> linkedList1 = new LinkedListPractice();
+    linkedList1.push(70);
+    linkedList1.push(30);
+    linkedList1.push(56);
+
+    linkedList1.display();
+
+}
+}
---
+++ src/Com/BridgeLbas/Node.java
@@ -0,0 +1,12 @@
+package Com.BridgeLbas;
+
+
+public class Node<E> {
+
+    E data;
+    Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+}
