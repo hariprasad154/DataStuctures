ee443aa6efe5173e14857a0a37f65aa43a8d1493
Hari Prasad G <hariprasas154@gmail.com>
2023-01-31 14:05:16 +0530

    pop - deleting the 1st eliment

--- src/Com/BridgeLbas/LinkedListPracticeMain.java
+++ src/Com/BridgeLbas/LinkedListPracticeMain.java
@@ -5,27 +5,32 @@ package Com.BridgeLbas;
 public class LinkedListPracticeMain {
 	public static void main(String[] args) {
 		
-	
 	LinkedListPractice<Integer> linkedList1 = new LinkedListPractice();
-    linkedList1.push(70);
+    System.out.println("the list-1");
+	linkedList1.push(70);
     linkedList1.push(30);
     linkedList1.push(56);
 
     linkedList1.display();
     
     LinkedListPractice<Integer> linkedList2 = new LinkedListPractice();
-    linkedList2.Append(70);
-    linkedList2.Append(30);
+    System.out.println("the list-2");
     linkedList2.Append(56);
+    linkedList2.Append(30);
+    linkedList2.Append(70);
     
     linkedList2.display();
     
     LinkedListPractice<Integer> linkedList3 = new LinkedListPractice();
+    System.out.println("the list-3");
     linkedList3.Append(70);
     linkedList3.Append(30);
     linkedList3.insert(56);
     
     linkedList3.display();
+    System.out.println("the list-2");
+    linkedList2.pop();
+    linkedList2.display();
     
 }
 }
